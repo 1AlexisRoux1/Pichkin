383576bfbf6af1b20fc8dd46aec85409032a532a
Mondon Arnaud <arnaud.mondon@mines-paristech.fr>
2020-01-31 13:25:34 +0100

    modif du nom

--- items.h
+++ items.h
@@ -1,37 +1,143 @@
 #include<iostream>
 #include<string>
+#include<time.h>
 using namespace std;
 
 
-class Item {
+class Objet {
     protected :
     string nom {};
     public :
-    Item (string n): nom(n){}
+    Objet (string n): nom(n){}
 
 };
 
-class Potion : public Item {
+class Potion : public Objet {
     private :
-    int pv;
+    int soin;
     int prix;
 
     public :
-    Potion (string n,int p,int pr): Item(n), prix(pr),pv(p){}
+    tableau={{"pomme","potion mystère"},{"pomme","pates","potion mystère"},{"super potion","potion","potion mystère"},{"super potion mystère"}};
+
+    Potion (string n,int p,int pr): Item(n), prix(pr),soin(p){}
+
+    Potion(int niveau){
+        int n = tableau[niveau].size();
+        x= rand()%n;
+        string a = tableau[niveau][x];
+        switch(a){
+            case "pomme":
+                soin =2;
+                prix=2;
+                break;
+            case "potion mystère":
+                int y = rand()10 -3
+                force =5;
+                prix=4;
+                break;
+            case "dague":
+                force =6;
+                prix=10;
+                break;
+            case "poèle":
+                force =7;
+                prix=10;
+                break;
+            case "four":
+                force =8;
+                prix=16;
+                break;
+            case "oreiller":
+                force =8;
+                prix=15;
+                break;
+            case "extincteur":
+                force =10;
+                prix=20;
+                break;
+            case "épée":
+                force =10;
+                prix=25;
+                break;
+            case "feux d'artifice":
+                force =13;
+                prix=50;
+                break;
+            case "palum":
+                force =15;
+                prix=75;
+                break;
+            
+        }
 };
 
-class Arme : public Item {
+class Arme : public Objet {
     private :
-    int force;
+    int attaque;
     int prix;
+    vector<vector<string>> tableau;
 
     public :
-    Arme(string n , int f, int pr): Item(n), force (f), prix(pr){}
+    tableau={{"baton","balais à chiotte"},{"baton","dague","poèle"},{"four","oreiller"},{"extincteur","épée"},{"feux d'artifice","palum"}};
+    
+    Arme(string n , int f, int pr): Item(n), attaque (f), prix(pr){}
 
+    Arme(int niveau){
+        int n = tableau[niveau].size();
+        x= rand()%n;
+        string a = tableau[niveau][x];
+        switch(a){
+            case "baton":
+                force =3;
+                prix=2;
+                break;
+            case "balais à chiotte":
+                force =5;
+                prix=4;
+                break;
+            case "dague":
+                force =6;
+                prix=10;
+                break;
+            case "poèle":
+                force =7;
+                prix=10;
+                break;
+            case "four":
+                force =8;
+                prix=16;
+                break;
+            case "oreiller":
+                force =8;
+                prix=15;
+                break;
+            case "extincteur":
+                force =10;
+                prix=20;
+                break;
+            case "épée":
+                force =10;
+                prix=25;
+                break;
+            case "feux d'artifice":
+                force =13;
+                prix=50;
+                break;
+            case "palum":
+                force =15;
+                prix=75;
+                break;
+            
+        }
+        
+        
+    }
+    
 
 };
 
-class Armure : public Item {
+class Armure : public Objet {
     private :
     int pa;
     int prix;
