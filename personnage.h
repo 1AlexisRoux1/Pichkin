a2053c794d91d5593b6ed84cac9e96269c51d427
Narjis21 <54950835+Narjis21@users.noreply.github.com>
2020-01-31 15:21:38 +0100

    Add files via upload

--- personnage.h
+++ personnage.h
@@ -20,30 +20,38 @@ class Heros : public Personnage {
     public:
     string nom;
     int niveau, gold;
-    Arme arme;
-    Armure armure;
-    vector<Potion*> equipement;
-    vector<int> inventaire;
+    int chance_cc;
+    int i = 0;
+    vector<Arme*> Armes;
+    vector<Armure*> Armures;
+    vector<int> inventaire_potion;
+
+
     Heros(string name, int lvl = 1) : Personnage(), niveau(lvl) {
-        arme = new Arme ("Branche d'arbre", 1, 2);
-        arme = new Armure ("Tutu rose", 1, 2);
+        Armes[i] = new Arme ("Branche d'arbre", 1, 2);
+        Armures[i] = new Armure ("Tutu rose", 1, 2);
         this->degats = (niveau-1)*2;
         this->pv = (niveau-1)*5 + 10;
         this->defense = niveau ;
     }    
+
+
     void monter_de_niveau() {
         niveau++;
         this->degats = (niveau-1)*2;
         this->pv = (niveau-1)*5 + 10;
         this->defense = niveau*1.5 ;
     }
-    void attaque(Ennemi cible) {
-        nb_rand = rand();
-
-
-        cible.pv = cible.pv - (this->degats + equipement[0]->attaque)
-
+    string attaque_ennemi(Personnage &cible) {
+        int pt_bonus = 0;
+        int nb_rand = rand();
+        if (nb_rand >= (1-chance_cc)) {
+            int pt_bonus = 1;
+        }
+        cible.pv = cible.pv - (this->degats + Armes[i]->attaque + pt_bonus);
+        return "Tiens, prends toi ça !";
     }
+
 };
 
 class Ennemi : public Personnage {
@@ -51,11 +59,22 @@ class Ennemi : public Personnage {
     int xp_loot;
     int gold_loot;
     string msg_attaque, msg_mort;
-    Ennemi(int pt_vie = 5, int dgt = 3, string message_attaque = "", string message_mort = "", int val_x = 0, int val_y = 0, int experience_donne = 1, int gold_loot = 2): 
-    x(val_x), y(val_y), pv = pt_vie, degats = dgt, msg_attaque = message_attaque, msg_mort = message_mort {}
-    void attaque_hero(Heros cible) {
-        cible.pv = cible.pv - (this->degats)
+
+    Ennemi(char c, int val_x, int val_y) {}
+
+    /* Ennemi(int pt_vie = 5, int dgt = 3, string message_attaque = "", string message_mort = "", int val_x = 0, int val_y = 0, int experience_donne = 1, int gold_loot = 2): 
+    Personnage(pt_vie, dgt), , msg_attaque(message_attaque), msg_mort(message_mort) {} */
+
+    string attaque_hero(Heros &cible) {
+        if ((this->degats - cible.defense)>=0) {
+            cible.pv = cible.pv - (this->degats -cible.defense);
+            return "Ouch";
+        }
     }
+    ~Ennemi () {
+        
+    }
+
 };
 
 #endif
